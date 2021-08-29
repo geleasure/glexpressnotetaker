53db6cd93f14c4186c1b6bef6a8d192e315ba0e4
geleasure <grleasure@gmail.com>
2021-08-29 15:18:03 -0400

    fixing all files heroku not working

--- utils/staticFile.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-// Static File Path for all files.
-const staticFilePath = (fileName) => {
-  const filePath = path.join(__dirname, "../public", `/${fileName}.html`);
-  return filePath;
-};
-
-module.exports = staticFilePath;
--- utils/writeToDb.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-// function which writes new notes to the db file.
-const writeToDb = (data) => {
-  const filePath = path.join(__dirname, "../db/db.json");
-  fs.writeFileSync(filePath, JSON.stringify(data));
-};
-
-module.exports = writeToDb;
